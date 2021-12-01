4eef448affb4e7c116c26498c2bec831a6150d8a
navinkumar.c <navinkumar.c@zohocorp.com>
2021-12-01 10:42:58 +0530

    sw caching

---
+++ src/sw.js
@@ -0,0 +1,33 @@
+
+var CACHE_NAME = 'resource-cache-v1';
+
+var urlsToCache = $$;
+
+this.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+
+})
+
+
+this.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+});
+
+this.addEventListener('periodicsync', event => {
+    if (event.tag == 'get-latest-news') {
+        console.log(event);
+        this.registration.showNotification("event")
+    }
+});
+
