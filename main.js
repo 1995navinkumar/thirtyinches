f276c8fb00b8b43b24a07389f7d308d270b4b3ee
navinkumar.c <navinkumar.c@zohocorp.com>
2021-12-19 16:39:41 +0530

    PORT

--- main.js
+++ main.js
@@ -63,6 +63,6 @@ app.use(errorLogger, function (err, req, res, next) {
 });
 
 // listen for request
-app.listen(process.env.port || 8800, function () {
-    console.log('server running on port', process.env.port || 8800);
+app.listen(process.env.PORT || 8800, function () {
+    console.log('server running on PORT', process.env.PORT || 8800);
 });
