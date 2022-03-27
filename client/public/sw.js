6111ad0db651ac0ddaa25ae9329748c33395c799
navinkumar.c <navinkumar.c@zohocorp.com>
2022-03-27 17:53:55 +0530

    notification icon

--- client/public/sw.js
+++ client/public/sw.js
@@ -2,24 +2,24 @@
 var CACHE_NAME = 'resource-cache-v1';
 
 var urlsToCache = [
-   "/",
-   "/404.html",
-   "/Lato/Lato-Black.ttf",
-   "/Lato/Lato-BlackItalic.ttf",
-   "/Lato/Lato-Bold.ttf",
-   "/Lato/Lato-BoldItalic.ttf",
-   "/Lato/Lato-Hairline.ttf",
-   "/Lato/Lato-HairlineItalic.ttf",
-   "/Lato/Lato-Italic.ttf",
-   "/Lato/Lato-Light.ttf",
-   "/Lato/Lato-LightItalic.ttf",
-   "/Lato/Lato-Regular.ttf",
-   "/Lato/OFL.txt",
-   "/css/app.css",
-   "/css/helper.css",
-   "/css/user-agent.css",
-   "/index.html",
-   "/js/bundle.js"
+    "/",
+    "/404.html",
+    "/Lato/Lato-Black.ttf",
+    "/Lato/Lato-BlackItalic.ttf",
+    "/Lato/Lato-Bold.ttf",
+    "/Lato/Lato-BoldItalic.ttf",
+    "/Lato/Lato-Hairline.ttf",
+    "/Lato/Lato-HairlineItalic.ttf",
+    "/Lato/Lato-Italic.ttf",
+    "/Lato/Lato-Light.ttf",
+    "/Lato/Lato-LightItalic.ttf",
+    "/Lato/Lato-Regular.ttf",
+    "/Lato/OFL.txt",
+    "/css/app.css",
+    "/css/helper.css",
+    "/css/user-agent.css",
+    "/index.html",
+    "/js/bundle.js"
 ];
 
 self.addEventListener("install", function (event) {
@@ -61,7 +61,12 @@ self.addEventListener('fetch', function (event) {
 self.onpush = function (event) {
     var message = event.data?.json();
     console.log(message);
-    self.registration.showNotification(message.title, message.options);
+    self.registration.showNotification(message.title, { ...defaultOptions, ...message.options });
     // From here we can write the data to IndexedDB, send it to any open
     // windows, display a notification, etc.
+}
+
+let defaultOptions = {
+    icon: "pwa/manifest-icon-512.maskable.png",
+    badge: "notification-icon.png"
 }
